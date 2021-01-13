0dd05efd7aaea8b28838b429a21264359c2f31a6
haniafirianti <haniafirianti@gmail.com>
2021-01-13 14:07:24 +0700

    first commit

---
+++ app/Http/Controllers/ImportController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Imports\ImportStudent;
+use App\Student;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ImportController extends Controller
+{
+    public function index(){
+    	$students = Student::all();
+    	return view('import', compact('students'));
+    }
+    public function Upload(Request $request){
+    	Excel::import(new ImportStudent(), $request->file('student'));
+    	return back();
+    }
+}
